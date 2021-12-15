9f732398930ff6e63ae1699ae467012e8abd1b72
Kevitachi <nicolas.cabrera.lettiere@gmail.com>
2021-12-15 02:25:34 -0300

    Automatic Build GAction

--- Build.py
+++ Build.py
@@ -1,7 +1,10 @@
 from optparse import OptionParser
-import subprocess
+import shlex, subprocess
 import os
 import sys
+import uuid
+from threading import Thread, Lock
+import tail, time
 
 parser = OptionParser()
 parser.add_option("-u", "--unity", dest="unity",
@@ -15,20 +18,29 @@ def print_help():
     parser.print_help()
     sys.exit()
 
-def unity_build():
+def unity_build(opts):
+    logfile = opts['destination'] + "/" + str(uuid.uuid4()) + ".txt"
+    command = opts['unity'] +" -quit -batchmode -logFile "+ logfile +" -projectPath "+ opts['project'] +" -executeMethod Build.build"
+    
+    p = subprocess.run(command.split(), shell=False, check=True, capture_output=True)
+    
+    with open(logfile, "r") as file:
+        for line in file:
+            print(line)
+    
+if __name__ == "__main__":
     (options, args) = parser.parse_args()
     defaults = vars(parser.get_default_values())
     optionsdict = vars(options)
    
-    if 'unity' not in optionsdict:
+    if not optionsdict['unity']:
         print('No se ha ingresado el ejecutable de Unity a usar\n')
         print_help()
-    if 'project' not in optionsdict:
-        print('No se ha ingresado el projecto de unity para buildear\n')
+    if not optionsdict['project']:
+        print('No se ha ingresado el proyecto de unity para buildear\n')
         print_help()
-    if 'destination' not in optionsdict:
+    if not optionsdict['destination']:
         print('No se ha ingresado el directorio de output\n')
         print_help()
         
-if __name__ == "__main__":
-    unity_build()
+    unity_build(optionsdict)
